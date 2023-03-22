544da907524a5442ada0259f60a96fe533266089
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-03-22 17:43:45 +0000

    Syntax update.

--- src/bash/ish.sh
+++ src/bash/ish.sh
@@ -3,5 +3,5 @@ apk update
 apk upgrade
 apk add bash
 apk add curl
-bash <(curl -Ls https://github.com/ltgcgo/painted-palette/raw/main/src/bash/install.sh)
+curl -Ls https://github.com/ltgcgo/painted-palette/raw/main/src/bash/install.sh | bash
 exit
