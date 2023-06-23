ee56538274b521831afd36cbbc009af6d9c36cfe
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-06-23 13:55:10 +0000

    Added an auto updater for Windows.

--- src/cmd/gui.cmd
+++ src/cmd/gui.cmd
@@ -1,6 +1,11 @@
 @echo off
 title Painted Palette Web UI
-move /Y patched.js deno.js
-cls
-.\deno.exe run --allow-net --allow-env --allow-read --allow-write deno.js batch
+:loop
+	if exist patched.js (
+		move /Y patched.js deno.js
+		echo Updated successfully.
+	)
+	.\deno.exe run --allow-net --allow-env --allow-read --allow-write deno.js batch
+	timeout /t 5 /nobreak > NUL
+goto loop
 exit
