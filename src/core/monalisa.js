e94733460d682387e2b219885c9c99e1abedeb99
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-07-22 09:06:02 +0000

    Updated credits.

--- src/core/monalisa.js
+++ src/core/monalisa.js
@@ -597,7 +597,7 @@ let Monalisa = class extends CustomEventSource {
 				"Authorization": session
 			}
 		});
-		if (sessionRep.status != 200) {
+		if (sessionRep?.status != 200) {
 			return sessionRep.statusText;
 		};
 		let sessionInfo = await sessionRep.json();
